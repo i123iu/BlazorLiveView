0365c3fb6d0c4dba8441644f99079ab18947fd2b
Adam Kundrát <ada.kundrat@gmail.com>
2025-10-08 23:26:27 +0200

    Sample app

---
+++ samples/BlazorLiveView.Sample/Program.cs
@@ -0,0 +1,39 @@
+using BlazorLiveView.Core.Extensions;
+using BlazorLiveView.Sample.Components;
+
+namespace BlazorLiveView.Sample;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.AddLiveView();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.MapLiveViewMirrorEndpoint();
+
+        app.Run();
+    }
+}
