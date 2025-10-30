6dc1bf9fa461c98f5dce1f2bb6eb49fd7f4e2cab
Adam Kundrát <ada.kundrat@gmail.com>
2025-10-30 13:49:34 +0100

    refactor: RenderTreeFrameBuilder

--- src/BlazorLiveView.Core/RenderTree/RenderTreeFrameBuilder.cs
+++ src/BlazorLiveView.Core/RenderTree/RenderTreeFrameBuilder.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 namespace BlazorLiveView.Core.RenderTree;
 
 /// <summary>
-/// Constructors of <see cref="RenderTreeFrame"/> is internal for Blazor. 
+/// Constructors of <see cref="RenderTreeFrame"/> are internal for Blazor. 
 /// This class uses reflection to access them. 
 /// </summary>
 internal static class RenderTreeFrameBuilder
@@ -18,170 +18,147 @@ internal static class RenderTreeFrameBuilder
     private static readonly ConstructorInfo _attributeConstructor;
     private static readonly ConstructorInfo _elementReferenceCaptureConstructor;
     private static readonly ConstructorInfo _componentReferenceCaptureConstructor;
-    private static readonly ConstructorInfo _defaultConstructor;
 
     static RenderTreeFrameBuilder()
     {
-        var constructors = typeof(RenderTreeFrame).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+        var constructors = typeof(RenderTreeFrame).GetConstructors(
+            BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public
+        );
 
-        // Default constructor for None frame type
-        _defaultConstructor = constructors.FirstOrDefault(c => c.GetParameters().Length == 0)!;
-
-        // Element constructor: (int sequence, int elementSubtreeLength, string elementName, object elementKey)
-        _elementConstructor = constructors.First(c =>
+        _elementConstructor = constructors.Single(c =>
         {
             var parameters = c.GetParameters();
-            return parameters.Length == 4 &&
-                   parameters[0].ParameterType == typeof(int) &&
-                   parameters[1].ParameterType == typeof(int) &&
-                   parameters[2].ParameterType == typeof(string) &&
-                   parameters[3].ParameterType == typeof(object);
+            return parameters.Length == 4
+                && parameters[0].ParameterType == typeof(int)
+                && parameters[1].ParameterType == typeof(int)
+                && parameters[2].ParameterType == typeof(string)
+                && parameters[3].ParameterType == typeof(object);
         });
 
-        // Component constructor: (int sequence, int componentSubtreeLength, Type componentType, ComponentState componentState, object componentKey)
-        _componentConstructor = constructors.First(c =>
+        _componentConstructor = constructors.Single(c =>
         {
             var parameters = c.GetParameters();
-            return parameters.Length == 5 &&
-                   parameters[0].ParameterType == typeof(int) &&
-                   parameters[1].ParameterType == typeof(int) &&
-                   parameters[2].ParameterType == typeof(Type) &&
-                   parameters[3].ParameterType == typeof(ComponentState) &&
-                   parameters[4].ParameterType == typeof(object);
+            return parameters.Length == 5
+                && parameters[0].ParameterType == typeof(int)
+                && parameters[1].ParameterType == typeof(int)
+                && parameters[2].ParameterType == typeof(Type)
+                && parameters[3].ParameterType == typeof(ComponentState)
+                && parameters[4].ParameterType == typeof(object);
         });
 
-        // Region constructor: (int sequence, int regionSubtreeLength)
-        _regionConstructor = constructors.First(c =>
+        _regionConstructor = constructors.Single(c =>
         {
             var parameters = c.GetParameters();
-            return parameters.Length == 2 &&
-                   parameters[0].ParameterType == typeof(int) &&
-                   parameters[1].ParameterType == typeof(int);
+            return parameters.Length == 2
+                && parameters[0].ParameterType == typeof(int)
+                && parameters[1].ParameterType == typeof(int);
         });
 
-        // Text/Markup constructor: (int sequence, bool isMarkup, string textOrMarkup)
-        _textMarkupConstructor = constructors.First(c =>
+        _textMarkupConstructor = constructors.Single(c =>
         {
             var parameters = c.GetParameters();
-            return parameters.Length == 3 &&
-                   parameters[0].ParameterType == typeof(int) &&
-                   parameters[1].ParameterType == typeof(bool) &&
-                   parameters[2].ParameterType == typeof(string);
+            return parameters.Length == 3
+                && parameters[0].ParameterType == typeof(int)
+                && parameters[1].ParameterType == typeof(bool)
+                && parameters[2].ParameterType == typeof(string);
         });
 
-        // Attribute constructor: (int sequence, string attributeName, object attributeValue, ulong attributeEventHandlerId, string attributeEventUpdatesAttributeName)
-        _attributeConstructor = constructors.First(c =>
+        _attributeConstructor = constructors.Single(c =>
         {
             var parameters = c.GetParameters();
-            return parameters.Length == 5 &&
-                   parameters[0].ParameterType == typeof(int) &&
-                   parameters[1].ParameterType == typeof(string) &&
-                   parameters[2].ParameterType == typeof(object) &&
-                   parameters[3].ParameterType == typeof(ulong) &&
-                   parameters[4].ParameterType == typeof(string);
+            return parameters.Length == 5
+                && parameters[0].ParameterType == typeof(int)
+                && parameters[1].ParameterType == typeof(string)
+                && parameters[2].ParameterType == typeof(object)
+                && parameters[3].ParameterType == typeof(ulong)
+                && parameters[4].ParameterType == typeof(string);
         });
 
-        // ElementReferenceCapture constructor: (int sequence, Action<ElementReference> elementReferenceCaptureAction, string elementReferenceCaptureId)
-        _elementReferenceCaptureConstructor = constructors.First(c =>
+        _elementReferenceCaptureConstructor = constructors.Single(c =>
         {
             var parameters = c.GetParameters();
-            return parameters.Length == 3 &&
-                   parameters[0].ParameterType == typeof(int) &&
-                   parameters[1].ParameterType == typeof(Action<ElementReference>) &&
-                   parameters[2].ParameterType == typeof(string);
+            return parameters.Length == 3
+                && parameters[0].ParameterType == typeof(int)
+                && parameters[1].ParameterType == typeof(Action<ElementReference>)
+                && parameters[2].ParameterType == typeof(string);
         });
 
-        // ComponentReferenceCapture constructor: (int sequence, Action<object> componentReferenceCaptureAction, int parentFrameIndex)
-        _componentReferenceCaptureConstructor = constructors.First(c =>
+        _componentReferenceCaptureConstructor = constructors.Single(c =>
         {
             var parameters = c.GetParameters();
-            return parameters.Length == 3 &&
-                   parameters[0].ParameterType == typeof(int) &&
-                   parameters[1].ParameterType == typeof(Action<object>) &&
-                   parameters[2].ParameterType == typeof(int);
+            return parameters.Length == 3
+                && parameters[0].ParameterType == typeof(int)
+                && parameters[1].ParameterType == typeof(Action<object>)
+                && parameters[2].ParameterType == typeof(int);
         });
     }
 
-    /// <summary>
-    /// Creates a None frame (uninitialized frame)
-    /// </summary>
-    public static RenderTreeFrame None(int sequence = 0)
-    {
-        var frame = _defaultConstructor is not null
-            ? (RenderTreeFrame)_defaultConstructor.Invoke([])
-            : new RenderTreeFrame();
+    public static RenderTreeFrame None()
+        => new();
 
-        // Use reflection to set the sequence and frame type since there's no public constructor
-        var sequenceField = typeof(RenderTreeFrame).GetField("SequenceField", BindingFlags.NonPublic | BindingFlags.Instance);
-        var frameTypeField = typeof(RenderTreeFrame).GetField("FrameTypeField", BindingFlags.NonPublic | BindingFlags.Instance);
+    public static RenderTreeFrame Element(
+        int sequence,
+        int elementSubtreeLength,
+        string elementName,
+        object? elementKey = null
+    ) => (RenderTreeFrame)_elementConstructor.Invoke([
+        sequence, elementSubtreeLength, elementName, elementKey
+    ]);
 
-        sequenceField!.SetValue(frame, sequence);
-        frameTypeField!.SetValue(frame, RenderTreeFrameType.None);
+    public static RenderTreeFrame Component(
+        int sequence,
+        int componentSubtreeLength,
+        Type componentType,
+        ComponentState? componentState = null,
+        object? componentKey = null
+    ) => (RenderTreeFrame)_componentConstructor.Invoke([
+        sequence, componentSubtreeLength, componentType, componentState, componentKey
+    ]);
 
-        return frame;
-    }
+    public static RenderTreeFrame Region(
+        int sequence,
+        int regionSubtreeLength
+    ) => (RenderTreeFrame)_regionConstructor.Invoke([
+        sequence, regionSubtreeLength
+    ]);
 
-    /// <summary>
-    /// Creates an Element frame
-    /// </summary>
-    public static RenderTreeFrame Element(int sequence, int elementSubtreeLength, string elementName, object? elementKey = null)
-    {
-        return (RenderTreeFrame)_elementConstructor.Invoke([sequence, elementSubtreeLength, elementName, elementKey]);
-    }
+    public static RenderTreeFrame Text(
+        int sequence,
+        string textContent
+    ) => (RenderTreeFrame)_textMarkupConstructor.Invoke([
+        sequence, false, textContent
+    ]);
 
-    /// <summary>
-    /// Creates a Component frame
-    /// </summary>
-    public static RenderTreeFrame Component(int sequence, int componentSubtreeLength, Type componentType, ComponentState? componentState = null, object? componentKey = null)
-    {
-        return (RenderTreeFrame)_componentConstructor.Invoke([sequence, componentSubtreeLength, componentType, componentState, componentKey]);
-    }
+    public static RenderTreeFrame Markup(
+        int sequence,
+        string markupContent
+    ) => (RenderTreeFrame)_textMarkupConstructor.Invoke([
+        sequence, true, markupContent
+    ]);
 
-    /// <summary>
-    /// Creates a Region frame
-    /// </summary>
-    public static RenderTreeFrame Region(int sequence, int regionSubtreeLength)
-    {
-        return (RenderTreeFrame)_regionConstructor.Invoke([sequence, regionSubtreeLength]);
-    }
+    public static RenderTreeFrame Attribute(
+        int sequence,
+        string attributeName,
+        object? attributeValue = null,
+        ulong attributeEventHandlerId = 0,
+        string? attributeEventUpdatesAttributeName = null
+    ) => (RenderTreeFrame)_attributeConstructor.Invoke([
+        sequence, attributeName, attributeValue, attributeEventHandlerId, attributeEventUpdatesAttributeName
+    ]);
 
-    /// <summary>
-    /// Creates a Text frame
-    /// </summary>
-    public static RenderTreeFrame Text(int sequence, string textContent)
-    {
-        return (RenderTreeFrame)_textMarkupConstructor.Invoke([sequence, false, textContent]);
-    }
+    public static RenderTreeFrame ElementReferenceCapture(
+        int sequence,
+        Action<ElementReference> elementReferenceCaptureAction,
+        string elementReferenceCaptureId
+    ) => (RenderTreeFrame)_elementReferenceCaptureConstructor.Invoke([
+        sequence, elementReferenceCaptureAction, elementReferenceCaptureId
+    ]);
 
-    /// <summary>
-    /// Creates a Markup frame
-    /// </summary>
-    public static RenderTreeFrame Markup(int sequence, string markupContent)
-    {
-        return (RenderTreeFrame)_textMarkupConstructor.Invoke([sequence, true, markupContent]);
-    }
-
-    /// <summary>
-    /// Creates an Attribute frame
-    /// </summary>
-    public static RenderTreeFrame Attribute(int sequence, string attributeName, object? attributeValue = null, ulong attributeEventHandlerId = 0, string? attributeEventUpdatesAttributeName = null)
-    {
-        return (RenderTreeFrame)_attributeConstructor.Invoke([sequence, attributeName, attributeValue, attributeEventHandlerId, attributeEventUpdatesAttributeName]);
-    }
-
-    /// <summary>
-    /// Creates an ElementReferenceCapture frame
-    /// </summary>
-    public static RenderTreeFrame ElementReferenceCapture(int sequence, Action<ElementReference> elementReferenceCaptureAction, string elementReferenceCaptureId)
-    {
-        return (RenderTreeFrame)_elementReferenceCaptureConstructor.Invoke([sequence, elementReferenceCaptureAction, elementReferenceCaptureId]);
-    }
-
-    /// <summary>
-    /// Creates a ComponentReferenceCapture frame
-    /// </summary>
-    public static RenderTreeFrame ComponentReferenceCapture(int sequence, Action<object> componentReferenceCaptureAction, int parentFrameIndex)
-    {
-        return (RenderTreeFrame)_componentReferenceCaptureConstructor.Invoke([sequence, componentReferenceCaptureAction, parentFrameIndex]);
-    }
+    public static RenderTreeFrame ComponentReferenceCapture(
+        int sequence,
+        Action<object> componentReferenceCaptureAction,
+        int parentFrameIndex
+    ) => (RenderTreeFrame)_componentReferenceCaptureConstructor.Invoke([
+        sequence, componentReferenceCaptureAction, parentFrameIndex
+    ]);
 }
