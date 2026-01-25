0bc06e6fc6c667418c63fac12d29c677bfa2a357
Adam Kundrát <ada.kundrat@gmail.com>
2026-01-25 15:38:13 +0100

    feat: CircuitTracker uses ConcurrentDictionary

--- src/BlazorLiveView.Core/Circuits/Services/CircuitTracker.cs
+++ src/BlazorLiveView.Core/Circuits/Services/CircuitTracker.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.RenderTree;
 using Microsoft.AspNetCore.Components.Server.Circuits;
 using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 
 namespace BlazorLiveView.Core.Circuits.Services;
 
@@ -13,12 +14,11 @@ internal sealed class CircuitTracker(
     public event ICircuitTracker.CircuitOpenedHandler? CircuitOpenedEvent;
     public event ICircuitTracker.CircuitClosedHandler? CircuitClosedEvent;
 
-    private readonly Lock _lock = new();
     private readonly ILogger<CircuitTracker> _logger = logger;
     private readonly ILoggerFactory _loggerFactory = loggerFactory;
 
-    private readonly Dictionary<string, ICircuit> _circuitsById = new();
-    private readonly Dictionary<Renderer, ICircuit> _circuitsByRenderer = new();
+    private readonly ConcurrentDictionary<string, ICircuit> _circuitsById = new();
+    private readonly ConcurrentDictionary<Renderer, ICircuit> _circuitsByRenderer = new();
 
     private readonly struct PendingMirrorCircuit(
         IUserCircuit source,
@@ -34,159 +34,157 @@ internal sealed class CircuitTracker(
     /// <summary>
     /// Clients that have sent a HTTP request to the mirror endpoint, but are not yet opened. 
     /// </summary>
-    private readonly Dictionary<string, PendingMirrorCircuit> _pendingMirrorCircuitsById = new();
+    private readonly ConcurrentDictionary<string, PendingMirrorCircuit> _pendingMirrorCircuitsById = new();
 
     public ICircuit? GetCircuit(string circuitId)
     {
-        lock (_lock)
-        {
-            return _circuitsById.GetValueOrDefault(circuitId);
-        }
+        _circuitsById.TryGetValue(circuitId, out var circuit);
+        return circuit;
     }
 
     public ICircuit? GetCircuit(RemoteRendererWrapper remoteRenderer)
     {
-        lock (_lock)
-        {
-            return _circuitsByRenderer.GetValueOrDefault(remoteRenderer.Inner);
-        }
+        _circuitsByRenderer.TryGetValue(remoteRenderer.Inner, out var circuit);
+        return circuit;
     }
 
     public IReadOnlyCollection<ICircuit> ListCircuits()
     {
-        lock (_lock)
-        {
-            return _circuitsById.Values.ToArray();
-        }
+        return _circuitsById.Values.ToArray();
     }
 
-    public void MirrorCircuitCreated(Circuit mirrorCircuit,
-        IUserCircuit sourceCircuit, IUserCircuit? parentCircuit, bool debugView)
+    public void MirrorCircuitCreated(
+        Circuit mirrorCircuit,
+        IUserCircuit sourceCircuit, 
+        IUserCircuit? parentCircuit, 
+        bool debugView
+    )
     {
-        lock (_lock)
+        if (_circuitsById.ContainsKey(mirrorCircuit.Id) ||
+            _pendingMirrorCircuitsById.ContainsKey(mirrorCircuit.Id))
         {
-            if (_circuitsById.ContainsKey(mirrorCircuit.Id) ||
-                _pendingMirrorCircuitsById.ContainsKey(mirrorCircuit.Id))
-            {
-                throw new InvalidOperationException(
-                    $"Circuit with id '{mirrorCircuit.Id}' is already tracked."
-                );
-            }
-
-            _logger.LogInformation(
-                "Mirror circuit created: {CircuitId}, source: {SourceCircuitId}, parent: {ParentCircuitId}",
-                mirrorCircuit.Id, sourceCircuit.Id, parentCircuit?.Id
+            throw new InvalidOperationException(
+                $"Circuit with id '{mirrorCircuit.Id}' is already tracked."
             );
-            PendingMirrorCircuit pending = new(sourceCircuit, parentCircuit, debugView);
-            _pendingMirrorCircuitsById.Add(mirrorCircuit.Id, pending);
+        }
+
+        _logger.LogDebug(
+            "Mirror circuit created: {CircuitId}, source: {SourceCircuitId}, parent: {ParentCircuitId}",
+            mirrorCircuit.Id, sourceCircuit.Id, parentCircuit?.Id
+        );
+
+        PendingMirrorCircuit pending = new(sourceCircuit, parentCircuit, debugView);
+        if (!_pendingMirrorCircuitsById.TryAdd(mirrorCircuit.Id, pending))
+        {
+            throw new InvalidOperationException(
+                $"Circuit with id '{mirrorCircuit.Id}' is already tracked."
+            );
         }
     }
 
     public void CircuitOpened(Circuit blazorCircuit)
     {
-        lock (_lock)
+        if (_circuitsById.ContainsKey(blazorCircuit.Id))
         {
-            if (_circuitsById.ContainsKey(blazorCircuit.Id))
-            {
-                throw new InvalidOperationException(
-                    $"Circuit with id '{blazorCircuit.Id}' is already tracked."
-                );
-            }
-
-            ICircuit circuit;
-            if (_pendingMirrorCircuitsById.Remove(blazorCircuit.Id, out var pending))
-            {
-                _logger.LogInformation("Mirror circuit opened: {CircuitId}",
-                    blazorCircuit.Id);
-
-                circuit = new MirrorCircuit(
-                    blazorCircuit,
-                    pending.source,
-                    pending.parent,
-                    DateTime.UtcNow,
-                    pending.debugView,
-                    _loggerFactory.CreateLogger<MirrorCircuit>()
-                );
-            }
-            else
-            {
-                _logger.LogInformation("User circuit opened: {CircuitId}",
-                    blazorCircuit.Id);
-
-                circuit = new UserCircuit(
-                    blazorCircuit,
-                    DateTime.UtcNow,
-                    _loggerFactory.CreateLogger<UserCircuit>()
-                );
-            }
-
-            _circuitsById.Add(blazorCircuit.Id, circuit);
-
-            CircuitWrapper circuitWrapped = new(blazorCircuit);
-            Renderer renderer = circuitWrapped.CircuitHost.Renderer.Inner;
-            _circuitsByRenderer.Add(renderer, circuit);
-
-            CircuitOpenedEvent?.Invoke(circuit);
+            throw new InvalidOperationException(
+                $"Circuit with id '{blazorCircuit.Id}' is already tracked."
+            );
         }
+
+        ICircuit circuit;
+        if (_pendingMirrorCircuitsById.TryRemove(blazorCircuit.Id, out var pending))
+        {
+            _logger.LogDebug("Mirror circuit opened: {CircuitId}",
+                blazorCircuit.Id);
+
+            circuit = new MirrorCircuit(
+                blazorCircuit,
+                pending.source,
+                pending.parent,
+                DateTime.UtcNow,
+                pending.debugView,
+                _loggerFactory.CreateLogger<MirrorCircuit>()
+            );
+        }
+        else
+        {
+            _logger.LogDebug("User circuit opened: {CircuitId}",
+                blazorCircuit.Id);
+
+            circuit = new UserCircuit(
+                blazorCircuit,
+                DateTime.UtcNow,
+                _loggerFactory.CreateLogger<UserCircuit>()
+            );
+        }
+
+        if (!_circuitsById.TryAdd(blazorCircuit.Id, circuit))
+        {
+            throw new InvalidOperationException(
+                $"Circuit with id '{blazorCircuit.Id}' is already tracked."
+            );
+        }
+
+        CircuitWrapper circuitWrapped = new(blazorCircuit);
+        Renderer renderer = circuitWrapped.CircuitHost.Renderer.Inner;
+        if (!_circuitsByRenderer.TryAdd(renderer, circuit))
+        {
+            throw new InvalidOperationException(
+                $"Circuit renderer is already tracked."
+            );
+        }
+
+        CircuitOpenedEvent?.Invoke(circuit);
     }
 
     public void CircuitUp(Circuit blazorCircuit)
     {
-        lock (_lock)
+        if (!_circuitsById.TryGetValue(blazorCircuit.Id, out var circuit))
         {
-            if (!_circuitsById.TryGetValue(blazorCircuit.Id, out var circuit))
-            {
-                throw new InvalidOperationException(
-                    $"Circuit with id '{blazorCircuit.Id}' is not tracked or open."
-                );
-            }
-
-            _logger.LogInformation("Circuit up: {CircuitId}", blazorCircuit.Id);
-            circuit.SetUp();
+            throw new InvalidOperationException(
+                $"Circuit with id '{blazorCircuit.Id}' is not tracked or open."
+            );
         }
+
+        _logger.LogDebug("Circuit up: {CircuitId}", blazorCircuit.Id);
+        circuit.SetUp();
     }
 
     public void CircuitDown(Circuit blazorCircuit)
     {
-        lock (_lock)
+        if (!_circuitsById.TryGetValue(blazorCircuit.Id, out var circuit))
         {
-            if (!_circuitsById.TryGetValue(blazorCircuit.Id, out var circuit))
-            {
-                throw new InvalidOperationException(
-                    $"Circuit with id '{blazorCircuit.Id}' is not tracked."
-                );
-            }
-
-            if (circuit.CircuitStatus == CircuitStatus.Down)
-            {
-                // Blazor can call CircuitDown twice when closing
-                return;
-            }
-
-            _logger.LogInformation("Circuit down: {CircuitId}", blazorCircuit.Id);
-            circuit.SetDown();
+            throw new InvalidOperationException(
+                $"Circuit with id '{blazorCircuit.Id}' is not tracked."
+            );
         }
+
+        if (circuit.CircuitStatus == CircuitStatus.Down)
+        {
+            // Blazor can call CircuitDown twice when closing
+            return;
+        }
+
+        _logger.LogDebug("Circuit down: {CircuitId}", blazorCircuit.Id);
+        circuit.SetDown();
     }
 
     public void CircuitClosed(Circuit blazorCircuit)
     {
-        lock (_lock)
+        if (!_circuitsById.TryRemove(blazorCircuit.Id, out var circuit))
         {
-            if (!_circuitsById.Remove(blazorCircuit.Id, out var circuit))
-            {
-                throw new InvalidOperationException(
-                    $"Circuit with id '{blazorCircuit.Id}' is not tracked or open."
-                );
-            }
-
-            _logger.LogInformation("Circuit closed: {CircuitId}", blazorCircuit.Id);
-            circuit.SetClosed();
-
-            CircuitWrapper circuitWrapped = new(blazorCircuit);
-            Renderer renderer = circuitWrapped.CircuitHost.Renderer.Inner;
-            _circuitsByRenderer.Remove(renderer);
-
-            CircuitClosedEvent?.Invoke(circuit);
+            throw new InvalidOperationException(
+                $"Circuit with id '{blazorCircuit.Id}' is not tracked or open."
+            );
         }
+
+        _logger.LogDebug("Circuit closed: {CircuitId}", blazorCircuit.Id);
+        circuit.SetClosed();
+
+        CircuitWrapper circuitWrapped = new(blazorCircuit);
+        Renderer renderer = circuitWrapped.CircuitHost.Renderer.Inner;
+        _circuitsByRenderer.TryRemove(renderer, out _);
+
+        CircuitClosedEvent?.Invoke(circuit);
     }
 }
