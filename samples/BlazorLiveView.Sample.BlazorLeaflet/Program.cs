1356618297b7f939e1b71c4791ca981f12473642
Adam Kundrát <ada.kundrat@gmail.com>
2026-03-11 14:42:28 +0100

    feat: BlazorLiveView.Sample.BlazorLeaflet

---
+++ samples/BlazorLiveView.Sample.BlazorLeaflet/Program.cs
@@ -0,0 +1,45 @@
+using BlazorLiveView.Core.Extensions;
+using BlazorLiveView.Sample.BlazorLeaflet.Components;
+
+namespace BlazorLiveView.Sample.BlazorLeaflet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddLiveView(options =>
+                 {
+                     options.ShowDebugOptions = true;
+                     options.InterceptJsInteropInvocations = true;
+                 });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.MapLiveViewMirrorEndpoint();
+
+            app.Run();
+        }
+    }
+}
