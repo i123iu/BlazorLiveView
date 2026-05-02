c92c2f70acc16064626ab12624e57950bf2f7a68
Adam Kundrát <ada.kundrat@gmail.com>
2026-05-02 13:33:57 +0200

    fix: incorrect window size when synced

--- src/BlazorLiveView.Core/Components/Tools/Position.cs
+++ src/BlazorLiveView.Core/Components/Tools/Position.cs
@@ -4,4 +4,14 @@ public struct Position(int x, int y)
 {
     public int x = x;
     public int y = y;
+
+    public static Position operator +(Position a, Position b)
+    {
+        return new Position(a.x + b.x, a.y + b.y);
+    }
+
+    public static Position operator -(Position a, Position b)
+    {
+        return new Position(a.x - b.x, a.y - b.y);
+    }
 }
